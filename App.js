90c0164e069b7ae6c195fa82fd0de748aba00817
danielshow <danielshotonwa53@gmail.com>
2019-08-08 14:52:52 +0100

    add images and make app scrollable

--- App.js
+++ App.js
@@ -7,16 +7,16 @@
  */
 
 import React, { Fragment } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
-    <Fragment>
+    <View style={{ flex: 1}}>
       <Header headerText={"Albums"} />
       <AlbumList />
-    </Fragment>
+    </View>
   );
 };
 
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,17 +7,20 @@ const AlbumDetail = ({ album }) => {
   return (
     <Card>
       <CardSection>
-        <View>
-
+        <View style={styles.thumbnailContainerStyle} >
+          <Image source={{ uri: album.thumbnail_image }} style={styles.thumbnailStyle} />
         </View>
         <View style={styles.textContainer}>
-          <Text>
+          <Text style={styles.textStyle}>
             {album.title} 
           </Text>
           <Text>
             { album.artist}
           </Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{uri: album.image}} style={styles.imageStyle} />
       </CardSection>
     </Card>
   );
@@ -27,6 +30,28 @@ const styles = {
   textContainer: {
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  
+  textStyle: {
+    fontSize: 18,
+  },
+
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    width: '100%',
+    height: 300,
   }
 }
+
 export default AlbumDetail;
